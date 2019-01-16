c9e0cb011f8a5ff4205c5f8f4e8b8355348c01e1
Andrew Junzki <andrew.junzki@yandex.com>
2019-01-17 00:22:55 +0800

    Update input reader.
    Reader now supports long strings.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <locale.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <poll.h>
 #include <unistd.h>
@@ -13,8 +14,7 @@
 void die(const char*);
 void bye(int);
 void sigint_handler(int);
-void check_locale();
-
+void get_input(char**, int, ssize_t);
 void idiot_core(char*);
 
 
@@ -22,12 +22,11 @@ int
 main(void)
 {
     signal(SIGINT, sigint_handler);
-    check_locale();
 
     int    retval;
 
-    ssize_t nread;
-    char    buf[BUFSIZ];
+    ssize_t count;  /* Total bytes available. */
+    char*   input_str = NULL;
 
     struct pollfd fdset = {
         .fd = STDIN_FILENO,
@@ -38,22 +37,44 @@ main(void)
 
     while (1)
     {
-        retval = poll(&fdset, 1, 1000);
+        count = 0;  /* Reset counter. */
+        retval = poll(&fdset, 1, 5000);
 
         if (!retval)
         {
             puts("hhhhhhhhhhh.");
             continue;
         }
-            
-        nread = 0;
-        memset(&buf, 0, BUFSIZ);  /* Flush buffer. */
 
-        nread = read(STDIN_FILENO, buf, BUFSIZ);
-        puts(buf);
+        ioctl(STDIN_FILENO, FIONREAD, &count);
+        get_input(&input_str, STDIN_FILENO, count);
+        puts(input_str);
     }
 
     return EXIT_SUCCESS;
+}
+
+
+void
+get_input(char** input_str, int fd, ssize_t count)
+{
+    ssize_t nread = 0;
+    ssize_t rolls = count / BUFSIZ + 1;
+    ssize_t index = 0;
+
+    *input_str = (char*)malloc(count);
+    if (NULL == *input_str)
+        die("Cannot allocate memory.");
+
+    memset(*input_str, 0, count);
+
+    for (index = 0; index < rolls; ++index)
+    {
+        ssize_t pos = index * BUFSIZ;
+        nread = read(fd, *input_str + pos, BUFSIZ);
+    }
+
+    (*input_str)[count] = '\0';  /* Append suffix flag. */
 }
 
 
@@ -62,23 +83,6 @@ die(const char *message)
 {
     perror(message);
     bye(EXIT_FAILURE);
-}
-
-
-void
-check_locale()
-{
-    char    *locale    = NULL;
-    char    *conv_flag = NULL;
-    ssize_t siz        = 0;
-
-    locale = setlocale(LC_CTYPE, "");
-    if (NULL == locale || (siz = strlen(locale)) == 0)
-        die("Locale not set.");
-
-    conv_flag = strstr(locale, "UTF-8");
-    if (NULL == conv_flag)
-        die("Bad locale, only UTF-8 supported.");
 }
 
 
